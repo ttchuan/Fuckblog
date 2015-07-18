6f257445b90d17e15f93c0fa0db9a1a3980bb08a
ttchuan <353808125@qq.com>
2015-07-18 23:41:30 +0800

    主页、注册、登录的跳转

--- app.js
+++ app.js
@@ -4,57 +4,82 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
-var users = require('./routes/users');
-
+var settings = require('./settings');
 var app = express();
-
-// view engine setup
+var port = normalizePort(process.env.PORT || '3000');
+var debug = require('debug')('blog:server');
+var http = require('http');
+var server = http.createServer(app);
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+app.set('port', port);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
+app.use(session({
+  secret:settings.cookieSecret,
+  key:settings.db,
+  cookie:{maxAge: 1000*60*60*24*30},
+  resave:true,
+  saveUninitialized:true,
+  store:new MongoStore({
+    db:settings.db,
+    host:settings.host,
+    port:settings.port
+  })
+}));
+routes(app);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
 module.exports = app;
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
---
+++ settings.js
@@ -0,0 +1,8 @@
+//配置信息
+//数据库连接信息
+module.exports = {
+	cookieSecret:'myblog',
+	db:'blog',
+	host:'localhost',
+	port:'27017'
+};
