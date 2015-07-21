91387997ad49659942b9368c8dd089aca984cf0c
ttchuan <353808125@qq.com>
2015-07-21 20:19:42 +0800

    添加MARKDOWN

--- models/post.js
+++ models/post.js
@@ -1,4 +1,5 @@
 var mongod = require('./db');
+var markdown = require('markdown').markdown;
 function Post (name,title,post) {
 	this.name = name;
 	this.title = title;
@@ -63,6 +64,9 @@ Post.get = function (name,callback) {
 				if (err) {
 					return callback(err);
 				};
+				docs.forEach(function (doc) {
+					doc.post = markdown.toHTML(doc.post);
+				})
 				callback(null,docs);
 			});
 		});
